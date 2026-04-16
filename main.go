b81dd466057ac4f5204a8d75fa607bce236b03de
aru <kairg.aru@gmail.com>
2026-04-16 23:16:46 +0600

    first commit

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
-
 import "fmt"
-
 func main() {
-	
-	fmt.Println("Я сегодня изучил:")
-	fmt.Println("Основы языка Go")
+	schooling:=11
+	fmt.Println(schooling)
+	schooling=12
+	fmt.Println(schooling)
+
+	name:="Vladislav"
+	fmt.Println(name)
+	name="Aru"
+	fmt.Println(name)
+
+	steps:=0
+	fmt.Println(steps)
+	steps=200
+	fmt.Println(steps)
+	fmt.Println("Хорошая работа! Вы уже на пути к своей ежедневной цели")
+
+	largeNumber:= 6000000
+	fmt.Println(largeNumber)
+
+	const breakTime := 15
+	fmt.Println(breakTime)
+	// const breakTime= 20
+	// fmt.Println(breakTime)
+	// конст нельзя поменять
+
 
 }
 
