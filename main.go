dda9bf255cf67da95c6d6909eff630862ced877c
aru <kairg.aru@gmail.com>
2026-04-19 19:33:51 +0600

    first commit

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 import "fmt"
 func main() {
+	// урок 4
+	fmt.Println("Я сегодня изучил:")
+	fmt.Println("Основы языка Go")
+
+	//урок 5
 	schooling:=11
 	fmt.Println(schooling)
 	schooling=12
@@ -20,11 +25,41 @@ func main() {
 	largeNumber:= 6000000
 	fmt.Println(largeNumber)
 
-	const breakTime := 15
-	fmt.Println(breakTime)
+	// const breakTime = 15
+	// fmt.Println(breakTime)
 	// const breakTime= 20
 	// fmt.Println(breakTime)
 	// конст нельзя поменять
+
+
+	// Урок 6
+	 age := 23
+	 fmt.Println(age)
+	 age= age + 1
+	 fmt.Println(age)
+
+
+	 height := 170
+	 height_in_meters := 1.7
+	 fmt.Println(height, height_in_meters)
+
+	 isStudent := true
+	 fmt.Println(isStudent)
+
+	 temperature := 25
+	 fmt.Println("Погода теплая:", temperature)
+
+
+	 favoriteQuote :="Отличный солнесный день!"
+	 fmt.Println(favoriteQuote)
+
+	  PI := 3.14
+	  fmt.Println(PI)
+	//   PI = "3.1415"
+	//   fmt.Println(PI)
+	//  нельзя присвоить строку переменной типа float64
+
+
 
 
 }
