ae81a0107fac0accc4582be2033fe0e04e56de39
aru <kairg.aru@gmail.com>
2026-04-19 20:41:49 +0600

    first commit

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
-import "fmt"
+import ("fmt"
+		"math")
 func main() {
 	// урок 4
 	fmt.Println("Я сегодня изучил:")
@@ -58,6 +59,59 @@ func main() {
 	//   PI = "3.1415"
 	//   fmt.Println(PI)
 	//  нельзя присвоить строку переменной типа float64
+
+	// Урок 7
+	bannerWidth:= 12
+	bannerHeight :=8
+	bannerArea := bannerHeight*bannerWidth
+	halfBannerArea := bannerArea/2
+	bannerBorderLength := (bannerWidth + bannerHeight) * 2
+	fmt.Println(bannerArea, halfBannerArea,bannerBorderLength)
+
+	boxCount :=29
+	leftoverBoxes:= boxCount % 5
+	fmt.Println(leftoverBoxes)
+
+	tempMorning:= 16
+	tempAfternoon := 20
+	tempEvening := 28
+	totalTemp:= tempAfternoon + tempEvening + tempMorning
+	averageTemp:= totalTemp/3
+	fmt.Println(totalTemp, averageTemp)
+
+	knownWords :=47
+	wordsGoal :=120
+	progressPercent:= float64(knownWords) / float64(wordsGoal) * 100
+	fmt.Println(progressPercent)
+
+	coins := 0
+	fmt.Println(coins)
+	coins+=500
+	fmt.Println(coins)
+	coins+=1200
+	fmt.Println(coins)
+	coins/=2
+	fmt.Println(coins)
+	coins*=2
+	fmt.Println(coins)
+	coins-=300
+	fmt.Println(coins)
+
+	participants := 42
+	groupCount :=8
+	participantsPerGroup:= participants/groupCount
+	fmt.Println(participantsPerGroup)
+
+	fmt.Println(20 - 4 * 3)
+	fmt.Println((20 - 4) * 3)
+
+
+	squareValue := 81
+	squareRoot := math.Sqrt(float64(squareValue))
+	multiplier := 5
+	exponent := 2
+	powerResult := math.Pow(float64(multiplier), float64(exponent))
+	fmt.Println(squareRoot,powerResult)
 
 
 
