32f244a70bd918d45391a7dcb681672273963774
aru <kairg.aru@gmail.com>
2026-04-21 17:27:21 +0600

    first commit

--- test.go
+++ test.go
@@ -83,7 +83,109 @@ func main() {
 	fmt.Println("toyList:", toyList)
 	fmt.Println("testToyList:", testToyList)
 
+// Урок 14
 
+
+	var arr [3][5]int
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 5; j++ {
+			fmt.Scan(&arr[i][j])
+		}
+	}
+	max := arr[0][0]
+	maxRow := 0
+	maxCol := 0
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 5; j++ {
+			if arr[i][j] > max {
+				max = arr[i][j]
+				maxRow = i
+				maxCol = j
+			}
+		}
+	}
+	fmt.Println(maxRow, maxCol)
+
+
+
+	var arr1 [11][11]string
+	for i := 0; i < 11; i++ {
+		for j := 0; j < 11; j++ {
+			arr1[i][j] = "."
+		}
+	}
+	mid := 11 / 2 
+	for i := 0; i < 11; i++ {
+		for j := 0; j < 11; j++ {
+			if i == mid || j == mid || i == j || i+j == 10 {
+				arr1[i][j] = "*"
+			}
+		}
+	}
+	for i := 0; i < 11; i++ {
+		for j := 0; j < 11; j++ {
+			fmt.Print(arr1[i][j], " ")
+		}
+		fmt.Println()
+	}
+
+
+	var board [8][8]string
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			if (i+j)%2 == 0 {
+				board[i][j] = "." 
+			} else {
+				board[i][j] = "*"
+			}
+		}
+	}
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			fmt.Print(board[i][j], " ")
+		}
+		fmt.Println()
+	}
+
+
+
+	var arr2 [4][4]int
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			fmt.Scan(&arr2[i][j])
+		}
+	}
+	var i, j int
+	fmt.Scan(&i, &j)
+	for col := 0; col < 4; col++ {
+		arr2[i][col], arr2[j][col] = arr2[j][col], arr2[i][col]
+	}
+	for r := 0; r < 4; r++ {
+		for c := 0; c < 4; c++ {
+			fmt.Print(arr2[r][c], " ")
+		}
+		fmt.Println()
+	}
+
+
+
+	var arr3 [4][4]int
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			fmt.Scan(&arr3[i][j])
+		}
+	}
+	var col1, col2 int
+	fmt.Scan(&col1, &col2)
+	for row := 0; row < 4; row++ {
+		arr3[row][col1], arr3[row][col2] = arr3[row][col2], arr3[row][col1]
+	}
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			fmt.Print(arr3[i][j], " ")
+		}
+		fmt.Println()
+	}
 
 }
 
